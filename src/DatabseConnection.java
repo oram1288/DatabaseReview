676b50b27166ae2b15f5ea183583c67cdcd856d7
oram1288 <cody.oram@keyin.com>
2024-07-18 16:25:03 -0230

    july18

--- src/DatabseConnection.java
+++ src/DatabseConnection.java
@@ -4,9 +4,9 @@ import java.sql.SQLException;
 
 public class DatabseConnection {
 
-     private static final String URL = "jdbc:postgresql://localhost:5432/DBReviewJava";
+     private static final String URL = "jdbc:postgresql://localhost:5432/DBreview";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "jordan1234";
+    private static final String PASSWORD = "leafsfan12";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
